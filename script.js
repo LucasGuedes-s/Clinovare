3d3d5c8184faf8a78884faeeda37d8b228548f1c
Glitch (glitch-hello-website) <none>
2024-01-02 13:57:24 +0000

    🍁🗃 Checkpoint ./cadastro.html:58483456/25 ./script.js:58483462/657 ./style.css:58483456/129

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ function obterDados(){
     req.onload = function(){ // Define um evento a ser executado quando a requisição for carregada com sucesso
         let users = req.response; // Armazena a resposta da requisição na variável 'users'
         console.log(users); // Exibe os dados recebidos no console
-        //exibirDados(users)
+        exibirDados(users)
     }
 }
 
 function exibirDados(jsonObj){
     //console.log(jsonObj); // Exibe a lista de usuários no console
 
-    let usuarios = jsonObj.data; // Extrai a propriedade 'data' do objeto JSON recebido e armazena em 'usuarios'
+    let usuarios = jsonObj; // Extrai a propriedade 'data' do objeto JSON recebido e armazena em 'usuarios'
     console.log(usuarios); // Exibe a lista de usuários no console
 
     usuarios.forEach(element =>{ // Itera sobre cada elemento da lista 'usuarios'
         //element.id; // Acessa a propriedade 'id' do elemento (não utilizado)
-        document.body.innerHTML += `ID:${element.id} </br>`; // Adiciona o ID do elemento ao corpo do documento HTML
-        document.body.innerHTML += `Nome:${element.first_name} </br>`; // Adiciona o nome do elemento ao corpo do documento HTML
+        document.body.innerHTML += `ID: ${element.id} </br>`; // Adiciona o ID do elemento ao corpo do documento HTML
+        document.body.innerHTML += `Nome: ${element.name} </br>`; // Adiciona o nome do elemento ao corpo do documento HTML
+        document.body.innerHTML += `Email: ${element.email} </br>`; // Adiciona o nome do elemento ao corpo do documento HTML
         document.body.innerHTML += `--------------------------------- </br>`; // Adiciona o nome do elemento ao corpo do documento HTML
 
     });
