71aab597e1b703c030a28f82c02f28223cd38bf4
Glitch (glitch-hello-website) <none>
2024-01-03 13:04:09 +0000

    🍡🐮 Checkpoint ./solicitacoes.html:58483456/2987 ./style.css:58483456/21816 ./script.js:58483456/3755 ./login.html:58483456/9418 ./cadastro.html:58483456/13343 ./SolicitarVacinas.html:58483456/5512 ./Solicitar.html:58483456/4615 ./Sobre.html:58483456/5110

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-function obterDados(){
-    const url = "https://jsonplaceholder.typicode.com/users"; 
-    let req = new XMLHttpRequest(); // Inicializa uma nova requisição HTTP
-    req.open("GET", url); // Abre uma conexão para fazer uma solicitação GET na URL especificada
-    req.responseType = "json"; // Define o tipo de resposta esperada como JSON
-    req.send(); // Envia a solicitação HTTP
+function obterDados() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  let req = new XMLHttpRequest(); // Inicializa uma nova requisição HTTP
+  req.open("GET", url); // Abre uma conexão para fazer uma solicitação GET na URL especificada
+  req.responseType = "json"; // Define o tipo de resposta esperada como JSON
+  req.send(); // Envia a solicitação HTTP
 
-    req.onload = function(){ // Define um evento a ser executado quando a requisição for carregada com sucesso
-        let users = req.response; // Armazena a resposta da requisição na variável 'users'
-        console.log(users);
-        exibirDados(users)
-    }
+  req.onload = function () {
+    // Define um evento a ser executado quando a requisição for carregada com sucesso
+    let users = req.response; // Armazena a resposta da requisição na variável 'users'
+    console.log(users);
+    exibirDados(users);
+  };
 }
 
-function exibirDados(jsonObj){
-    let usuarios = jsonObj;
+function exibirDados(jsonObj) {
+  let usuarios = jsonObj;
 
-    usuarios.forEach(element =>{ // Itera sobre cada elemento da lista 'usuarios'
-      
-        let usuarioDiv = document.createElement('div'); //Criação de uma nova DIV
-        usuarioDiv.classList.add('usuarioInfo'); //Adição de uma classe a essa div
-      
-        usuarioDiv.innerHTML = `
+  usuarios.forEach((element) => {
+    // Itera sobre cada elemento da lista 'usuarios'
+
+    let usuarioDiv = document.createElement("div"); //Criação de uma nova DIV
+    usuarioDiv.classList.add("usuarioInfo"); //Adição de uma classe a essa div
+
+    usuarioDiv.innerHTML = `
         <p>ID: ${element.id}</p>
         <p>Nome: ${element.name}</p>
         <p>Email: ${element.email}</p>
         <p>---------------------------------</p>
-        <input type="button" value="Confirmar presença" onclick="confirmacao()">`;  
-    document.getElementById('listaUsuarios').appendChild(usuarioDiv);
-
-    });
+        <input type="button" value="Confirmar presença" onclick="confirmacao()">`;
+    document.getElementById("listaUsuarios").appendChild(usuarioDiv);
+  });
 }
 
 function enviarDados() {
   const formulario = document.getElementById("formUser");
   const formData = new FormData(formulario);
-  
+
   const data = Object.fromEntries(formData); // Converte os dados do FormData em um objeto JavaScript
-  console.log(data)
-  
+  console.log(data);
+
   fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json()).then(data => console.log(data));
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
-function confirmacao(){
-  alert(`Confirmado`)
-  
+function confirmacao() {
+  alert(`Confirmado`);
 }
